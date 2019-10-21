afc7b424b8cbfeedb777d5861859a73c3ff5d063
hd <ha_do@yahoo.com>
2019-10-21 15:26:52 -0700

    change CategoryViewController too XXXCategoryViewconroller.swift   and ItemViewController.swift to CategoryViewController.swift !  still can display hard-coded things in the table

--- testxcodecompletion/Controllers/CategoryViewController.swift
+++ testxcodecompletion/Controllers/CategoryViewController.swift
@@ -1,51 +1,38 @@
 //
-//  CategoryViewController.swift
+//  ViewController.swift
 //  testxcodecompletion
 //
 //  Created by ha tuong do on 10/21/19.
 //  Copyright © 2019 alpine. All rights reserved.
 //
 
-// import Foundation
 import UIKit
-import RealmSwift
-// import ChameleonFramework
 
-//class CategoryViewController: UITableViewController {
-//    
-//    let realm = try! Realm()
-//    
-//    var categories: Results<Category>?
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        loadCategories()
-//        tableView.separatorStyle = .none
+class CategoryViewController: UITableViewController {
+
+    
+    var todoItems = ["king", "queen", "princess", "prince", "lord"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print ("able to do xcode completion when start typing tableview... and todoItems.count = ", todoItems.count)
+        return todoItems.count 
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+      
+        // let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = todoItems[indexPath.row]
+        return cell
+        
+    }
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 //    }
-//    
-//    func loadCategories() {
-//        categories  = realm.objects(Category.self)
-//        tableView.reloadData()
-//    }
-//
-//    //MARK: - TableView Datasource Methods
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        
-//        return categories?.count ?? 1
-//    }
-//
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        
-//        let cell = super.tableView(tableView, cellForRowAt: indexPath)
-//        
-//        if let category = categories?[indexPath.row] {
-//            cell.textLabel?.text = category.name
-//            guard let categoryColour = UIColor(named: category.colour) else {fatalError()}
-//            
-//            cell.backgroundColor = categoryColour
-//            cell.textLabel?.textColor = UIColor.green
-//        }
-//       return cell
-//    }
-//
-//}
+
+}
+
